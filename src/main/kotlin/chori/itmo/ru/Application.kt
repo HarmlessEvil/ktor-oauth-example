3d7a204fd9ebec79cbd97a9815aa83eadb0d91c9
Alexander Chori <alexandrchori@gmail.com>
2021-04-05 21:23:59 +0300

    Initial commit

---
+++ src/main/kotlin/chori/itmo/ru/Application.kt
@@ -0,0 +1,11 @@
+package chori.itmo.ru
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import chori.itmo.ru.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
